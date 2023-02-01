8e0c2f16f1cf99dd90b422ef2369447ed5d6079a
Vic Godoy <61629307+mvigodoy@users.noreply.github.com>
2023-02-01 17:18:17 -0300

    corregi error de CORS en el back

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM amazoncorretto:11-alpine-jdk
 MAINTAINER mvg
-COPY src/target2/mvg-0.0.1-SNAPSHOT.jar mvg-app.jar
+COPY target/mvg-0.0.1-SNAPSHOT.jar mvg-app.jar
 ENTRYPOINT ["java","-jar","mvg-app.jar"]
--- src/main/java/com/portfolio/mvg/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mvg/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "https://frontendmvg.web.app")
+@CrossOrigin (origins = {"https://frontendmvg.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
